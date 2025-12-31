ff74163a80d45eb1337a78a3a38aa243dfe6feae
MD. FAYSAL KHAN <22-49312-3@student.aiub.edu>
2025-12-31 13:50:39 +0600

    sir repo added

---
+++ Final/Section_Q/View/dashboard.php
@@ -0,0 +1,23 @@
+
+<?php 
+session_start();
+
+$isLoggedIn = $_SESSION["isLoggedIn"] ?? false;
+if (!$isLoggedIn) {
+    Header("Location: login.php");
+}
+
+$email = $_SESSION["email"] ??"";
+
+?>
+
+<html>
+
+<body>
+    <h1>This is dashboard</h1>
+    <h2>Welcome <?php echo $email;?></h2>
+    <!-- <img src="../uploads/blue.png" /> -->
+    <a href="..\Controller\logout.php">Logout</a>
+    <a href="users.php">Users</a>
+</body>
+</html>
